0af3d8ef6ae27da586bf298b05f57a465f7f6873
AtakOskonbaev <atakoskonbaev21@gmail.com>
2026-05-11 22:21:07 +0600

    changed app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Привет из Railway!</h1>'
+    return '<h1>Railway deployed!</h1><p>auto-deploy from github</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
